a2b2c55ae47b1a9663d72e10be7d3da91aee20ad
Emrah Atalay <emrahatalay92@gmail.com>
2024-11-28 14:48:44 +0300

    Add caching mechanism for XML data and update dependencies
    Modified using directives to include System.Xml.Linq.
    Added private field _lastCheckTime of type DateTime.
    Added public property DataCacheMinute of type short.
    Changed currencyList from an empty array to an empty list.
    Added private method GetXmlDataList for caching XML data.

--- TCMBRatesClient/TCMBClient/TcmbClient.cs
+++ TCMBRatesClient/TCMBClient/TcmbClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
 using TCMBRatesClient.Models;
 
 namespace TCMBRatesClient.TCMBClient;
@@ -7,7 +8,12 @@ public class TcmbClient(HttpClient httpClient) : TcmbClientBase
 {
     private const string HouryBaseUrl = "https://www.tcmb.gov.tr/reeskontkur/";
     private const string TodayBaseUrl = "https://www.tcmb.gov.tr/kurlar/today.xml";
-        
+
+    private IEnumerable<Currency> currencyList = [];
+    private DateTime _lastCheckTime;
+
+    public short DataCacheMinute { get; set; }
+
     public override async Task<TcmbResponse?> GetHourlyRatesAsync(DateTime dateTime)
     {
         dateTime = GetDateTime(dateTime);
@@ -50,5 +56,30 @@ public class TcmbClient(HttpClient httpClient) : TcmbClientBase
         if (serializer.Deserialize(reader) is not Currency[] result) return [];
 
         return result;
+    }
+
+    private IEnumerable<Currency> GetXmlDataList()
+    {
+        if ((currencyList != null && currencyList.Any()) && (DateTime.Now - _lastCheckTime).TotalMinutes < DataCacheMinute)
+            return currencyList;
+
+        XDocument xmlDocument = XDocument.Load(TodayBaseUrl);
+
+        currencyList = xmlDocument.Descendants("Currency")
+                               .Select(e => new Currency
+                               {
+                                   CurrencyCode = e.Attribute("CurrencyCode")?.Value ?? "",
+                                   CurrenyName = e.Element("CurrencyName")?.Value ?? "",
+                                   Isim = e.Element("Isim")?.Value ?? "",
+                                   Unit = int.Parse(e.Element("Unit")?.Value ?? "0"),
+                                   ForexBuying = decimal.TryParse(e.Element("ForexBuying")?.Value.Replace(".", ","), out decimal FbPrice) ? FbPrice : 0,
+                                   ForexSelling = decimal.TryParse(e.Element("ForexSelling")?.Value.Replace(".", ","), out decimal FsPrice) ? FsPrice : 0,
+                                   BanknoteBuying = decimal.TryParse(e.Element("BanknoteBuying")?.Value.Replace(".", ","), out decimal BbPrice) ? BbPrice : null,
+                                   BanknoteSelling = decimal.TryParse(e.Element("BanknoteSelling")?.Value.Replace(".", ","), out decimal BsPrice) ? BsPrice : null,
+                               });
+
+        _lastCheckTime = DateTime.Now;
+
+        return currencyList;
     }
 }
